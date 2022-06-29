9552c4a11ce096db121a74b94d6453d33d20ed97
Gavin Ballard <gavin@gavinballard.com>
2022-06-29 10:52:43 +1000

    Refactor upsells form

--- src/modules/upsells/components/upsell.jsx
+++ src/modules/upsells/components/upsell.jsx
@@ -1,42 +1,9 @@
 import { useState } from "preact/compat";
-import { UpsellButton } from "./upsell_button";
+import { UpsellForm } from "./upsell_form";
+import { formatAmount } from "../../../lib/helpers";
 
 export const Upsell = ({ submarine, upsell }) => {
   const [selectedVariantIndex, setSelectedVariantIndex] = useState(0);
-  const [quantity, setQuantity] = useState(1);
-  const [loading, setLoading] = useState(false);
-
-  let variantSelector = null;
-  if(upsell.variants.length > 1) {
-    variantSelector = (
-      <select onChange={(e) => setSelectedVariantIndex(e.target.value)}>
-        {upsell.variants.map((variant, index) => {
-          return <option key={variant.id} value={index}>{variant.title}</option>;
-        })}
-      </select>
-    );
-  }
-
-  let quantitySelector = null;
-  quantitySelector = (
-    <div className="fieldset">
-      <div className="field field--required field--show-floating-label">
-        <div className="field__input-wrapper">
-          <label className="field__label field__label--visible">Quantity</label>
-          <input
-            onChange={(e) => setQuantity(parseInt(e.target.value))}
-            placeholder="Quantity"
-            className="field__input field__in"
-            aria-required="true"
-            type="number"
-            value="1"
-            name=""
-          />
-        </div>
-      </div>
-    </div>
-  );
-
   const selectedVariant = upsell.variants[selectedVariantIndex];
 
   return (
@@ -50,20 +17,19 @@ export const Upsell = ({ submarine, upsell }) => {
       <div className="submarine-upsells--price-wrapper">
         <h4>{upsell.title}</h4>
         <p>
-          <del>{upsell.price}</del> <span>58%</span>
+          <del>{formatAmount(upsell.price)}</del> <span>58%</span>
         </p>
         <p>
-          <strong>{upsell.price}</strong>
+          <strong>{formatAmount(upsell.price)}</strong>
         </p>
       </div>
 
-      <div className="submarine-upsells--action-wrapper">
-        {variantSelector}
-        {quantitySelector}
-        <UpsellButton
+      <div className="submarine-upsells--form-wrapper">
+        <UpsellForm
           submarine={submarine}
+          upsell={upsell}
           selectedVariant={selectedVariant}
-          quantity={quantity}
+          setSelectedVariantIndex={setSelectedVariantIndex}
         />
       </div>
     </div>
